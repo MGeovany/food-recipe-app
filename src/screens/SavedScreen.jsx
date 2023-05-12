c9016dc26e436318399584600479f8cd6c32c9dc
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2023-05-12 16:32:15 -0600

    feat: added back button save screen

--- src/screens/SavedScreen.jsx
+++ src/screens/SavedScreen.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { ScrollView, Text, View } from "react-native";
 import { styles } from "../styles";
+import { SavedList } from "../components/SavedList";
+import Icon from "react-native-vector-icons/AntDesign";
 
 export const SavedScreen = ({ navigation }) => {
   return (
     <ScrollView>
       <View style={styles.styledContainer}>
-        <View style={styles.pageTitleContainer}>
+        <Icon
+          name="back"
+          size={30}
+          onPress={() => navigation.navigate("Home")}
+          style={styles.iconBack}
+        />
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+          }}
+        >
           <Text style={styles.pageTitle}>Recetas</Text>
           <Text
             style={{
@@ -20,6 +34,7 @@ export const SavedScreen = ({ navigation }) => {
           </Text>
           <Text style={styles.pageTitle}>para mas tarde</Text>
         </View>
+        <SavedList />
       </View>
     </ScrollView>
   );
