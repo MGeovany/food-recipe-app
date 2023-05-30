3dcb6d5f6e05c96133a4a1196764dce595ddbafe
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2023-05-30 17:08:55 -0600

    feat: removed fragment

--- src/routes/HomeStackScreen.jsx
+++ src/routes/HomeStackScreen.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { HomeScreen } from "../screens/HomeScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { DetailsScreen } from "../screens/DetailsScreen";
@@ -8,15 +7,19 @@ const HomeStack = createNativeStackNavigator();
 
 export const HomeStackScreen = () => {
   return (
-    <>
-      <HomeStack.Navigator
-        screenOptions={{
-          headerShown: false,
+    <HomeStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <HomeStack.Screen name="HomeScreen" component={HomeScreen} />
+      <HomeStack.Screen
+        name="DetailsScreen"
+        component={DetailsScreen}
+        options={{
+          tabBarStyle: { display: "none" },
         }}
-      >
-        <HomeStack.Screen name="HomeScreen" component={HomeScreen} />
-        <HomeStack.Screen name="DetailsScreen" component={DetailsScreen} />
-      </HomeStack.Navigator>
-    </>
+      />
+    </HomeStack.Navigator>
   );
 };
